c80fc87544c2fc37d3d13b73de5792e2b237c5b2
akhun <akhun@AZIZKHAN>
2023-12-14 18:21:50 +0500

    Добавьте файлы проекта.

---
+++ RealTimeSignalR/Program.cs
@@ -0,0 +1,31 @@
+using RealTimeSignalR.Components;
+using RealTimeSignalR.Hubs;
+using Microsoft.AspNetCore.ResponseCompression;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+          new[] { "application/octet-stream" });
+});
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+app.UseResponseCompression();
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+app.MapHub<ChatHub>("/chathub");
+app.Run();
+
